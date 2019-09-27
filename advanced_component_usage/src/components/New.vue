ec9079b6843f85a6b090fe75bb34ba2f47bf9925
Kenneth Lee <kyung.kenneth.lee@gmail.com>
2019-09-26 18:31:55 -0700

    understanding dynamic component behavior

--- advanced_component_usage/src/components/New.vue
+++ advanced_component_usage/src/components/New.vue
@@ -1,11 +1,22 @@
 <template>
     <div>
         <h3>New Quote</h3>
+        <button @click="counter++">Increase</button>
+        <p>{{counter}}</p>
     </div>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                counter: 0,
+            }
+        },
+        destroyed() {
+            //The component is destroyed and recreated dynamically
+            console.log('Destroyed!');
+        }
     }
 </script>
 
