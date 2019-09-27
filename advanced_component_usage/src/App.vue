21ef33ae4231542fd40a2e379d888634b76579e4
Kenneth Lee <kyung.kenneth.lee@gmail.com>
2019-09-26 18:05:17 -0700

    set up for dynamic components

--- advanced_component_usage/src/App.vue
+++ advanced_component_usage/src/App.vue
@@ -2,8 +2,18 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
+
+                <button @click="selectedComponent = 'appQuote'" >Quote</button>
+                <button @click="selectedComponent = 'appAuthor'">Author</button>
+                <button @click="selectedComponent = 'appNew'">New</button>
+
+                <hr>
+                <p>{{ selectedComponent }}</p>
+
                 <app-quote >
                     <h1 slot="title">{{quoteTitle}}</h1>
+
+
                     <div slot="content">This is the quote</div>
 
                     <p> This paragraph is for the default slot</p>
@@ -16,15 +26,20 @@
 <script>
 
     import Quote from './components/Quote.vue';
+    import Author from './components/Author.vue';
+    import New from './components/New.vue';
 
     export default {
         data() {
             return {
-                quoteTitle: 'The Quote'
+                quoteTitle: 'The Quote',
+                selectedComponent: 'appQuote' //match the appQuote selector
             }
         },
         components: {
             appQuote: Quote,
+            appAuthor: Author,
+            appNew: New,
         }
     }
 </script>
