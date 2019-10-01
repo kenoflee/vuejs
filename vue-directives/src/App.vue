134789c096b0750b025a1a5c0f9c09d9299ff58b
Kenneth Lee <kyung.kenneth.lee@gmail.com>
2019-10-01 08:54:59 -0700

    create directive modifier

--- vue-directives/src/App.vue
+++ vue-directives/src/App.vue
@@ -12,7 +12,7 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custom Directives</h1>
                 <!-- has argument of 'background' -->
-                <p v-highlight:background="'red'">Color this</p>
+                <p v-highlight:background.delayed="'red'">Color this</p>
                 <!-- has no argument -->
                 <p v-highlight="'red'">Color this</p>
             </div>
--- vue-directives/src/main.js
+++ vue-directives/src/main.js
@@ -4,11 +4,19 @@ import App from './App.vue'
 Vue.directive('highlight', {
   //these are the lifecycle hooks for directives
   bind(el, binding, vnode) {
-    if(binding.arg == 'background') {
-      el.style.backgroundColor = binding.value;
-    } else {
-      el.style.color = binding.value;
-    }
+    let delay = 0;
+    if(binding.modifiers['delayed']) {
+      delay = 3000;
+    };
+
+    setTimeout(()=> {
+      if(binding.arg == 'background') {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    }, delay)
+    
   }
 
 }); //v-highlight
