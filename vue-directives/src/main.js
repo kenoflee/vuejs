bdc0cca6eb614d1bd89ae5187abf002981c4bd3a
Kenneth Lee <kyung.kenneth.lee@gmail.com>
2019-10-01 08:39:44 -0700

    add basic global directive

--- vue-directives/src/main.js
+++ vue-directives/src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.directive('highlight', {
+  //these are the lifecycle hooks for directives
+  bind(el, binding, vnode) {
+    el.style.backgroundColor = 'green';
+  }
+
+}); //v-highlight
+
 new Vue({
   el: '#app',
   render: h => h(App)
