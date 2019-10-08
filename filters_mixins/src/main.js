ea1619340e44cb2c335559db901b4eb793c62da5
Kenneth Lee <kennethlee@Kenneths-MacBook-Pro.local>
2019-10-08 12:56:39 -0700

    creating global mixins

--- filters_mixins/src/main.js
+++ filters_mixins/src/main.js
@@ -5,6 +5,20 @@ Vue.filter('to-lowercase', (string) => {
   return string.toLowerCase();
 })
 
+/**
+ * In this example, global mixin gets invoked 3 times
+ * 
+ * 1. in the new Vue() instance
+ * 2. in the App.vue
+ * 3. in the List.vue
+ */
+
+Vue.mixin({
+  created() {
+    console.log('Global Mixin - Created Hook');
+  },
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
